71b5b652c5582c1a569ab184ed6e7f0558437e2a
beksss <lmonarh.996@gmail.com>
2025-03-04 08:51:25 +0600

    added basic security configs

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated() // Все запросы требуют аутентификации
+                )
+                .formLogin() // Включаем форму логина
+                .and()
+                .logout(); // Включаем логаут
+        return http.build();
+    }
+}
